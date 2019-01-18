ac2117378260cb3b543c21b52454ce7ae8b825db
Denis Kotlyarchuk <kotlyarchukd@gmail.com>
2019-01-18 08:15:34 +0200

    Only authorized users can see New thread link in nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,9 +40,11 @@
                         <li class="nav-item">
                             <a class="nav-link" href="/threads/">All Threads</a>
                         </li>
+                        @if (auth()->check())
                         <li class="nav-item">
-                            <a class="nav-link" href="/threads/create">Create Thread</a>
+                            <a class="nav-link" href="/threads/create">New Thread</a>
                         </li>
+                        @endif
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
                                 aria-expanded="false">
